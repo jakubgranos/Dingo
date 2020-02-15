0fd52a6444bbd8a06d065493a0223a7d0f6fc2f2
Jakub <jakubgranos@gmail.com>
2020-02-15 22:58:24 +0100

    optymize indent in section menu file

--- theme-parts/sections/section_menu.php
+++ theme-parts/sections/section_menu.php
@@ -9,6 +9,6 @@ $background_color = get_sub_field('background_color'); ?>
 
 <section style="background-color: <?php echo $background_color; ?>; min-height: 400px; padding: 20px;">
   <?php if (!empty( $title )): ?>
-	  <h2><?php echo $title; ?>
+    <h2><?php echo $title; ?>
   <?php endif; ?>
 </section> 
