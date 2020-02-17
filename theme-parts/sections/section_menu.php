a5fd21d8edcb95a0a11f47d77da7b733b2825853
Jakub <jakubgranos@gmail.com>
2020-02-17 22:08:18 +0100

    delete unnecessary variables

--- theme-parts/sections/section_menu.php
+++ theme-parts/sections/section_menu.php
@@ -4,14 +4,10 @@
   @package Dingo
 */
 
-$title = get_sub_field('title');
-$subtitle  = get_sub_field('sub_title');
-$background_color = get_sub_field('background_color'); 
-?>
+$title = get_sub_field('title');?>
 
-<section style="background-color: <?php echo $background_color; ?>; min-height: 400px; padding: 20px;">
-  <?php if (!empty( $title )): ?>
+<section>
+  <?php if (!empty($title)): ?>
     <h2><?php echo $title; ?>
-    <p><?php echo $subtitle;?></p>
-  <?php endif; ?>
-</section> 
+  <?php endif; ?>   
+</section>
