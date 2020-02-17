66d73d2dc7c1a70b6043d10ee67970fe266022bc
Jakub <jakubgranos@gmail.com>
2020-02-17 20:38:42 +0100

    chagne if statment on switch and delete comments in file page.php

--- page.php
+++ page.php
@@ -1,25 +1,19 @@
 <?php
 get_header();
-require_once('theme-parts/header/header.php');
-// Check value exists.
-if( have_rows('sections') ):
-  // Loop through rows.
-  while ( have_rows('sections') ) : the_row();
-    // Case: Paragraph layout.
-    if( get_row_layout() == 'section_menu' ):
-      require_once('theme-parts/sections/section_menu.php');
-      // Do something...
-      // Case: Download layout.
-    elseif( get_row_layout() == 'section_contact' ): 
-      require_once('theme-parts/sections/section_contact.php');
-      // Do something...
-    endif;
-    // End loop.
-  endwhile;
 
-  // No value.
-  else :
-    // Do something...
+require_once('theme-parts/header/header.php');
+
+if( have_rows('sections') ):
+  while ( have_rows('sections') ):
+  switch(the_row()) {
+    case get_row_layout() == 'section_menu':
+      require_once('theme-parts/sections/section_menu.php');
+    break;
+    case get_row_layout() == 'section_contact':
+      require_once('theme-parts/sections/section_contact.php');
+    break;
+  }
+  endwhile;
 endif;
 
 get_footer();
