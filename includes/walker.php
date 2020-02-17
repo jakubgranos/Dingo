4b1113635c32fc12f4f7c714a26412cfb8af7cf9
Jakub <jakubgranos@gmail.com>
2020-02-17 19:39:59 +0100

    improvement indents in line 41 in walker.php

--- includes/walker.php
+++ includes/walker.php
@@ -38,7 +38,7 @@ class Dingo_Walker_header_nav extends  Walker_Nav_menu {
 	
     $output .= $indent . '<li' . $id . $value . $class_names . $li_attributes . '>';
 	
-		$attributes = ! empty($item->attr_title) ? ' title="' . esc_attr($item->attr_title) . '"' : '';
+    $attributes = ! empty($item->attr_title) ? ' title="' . esc_attr($item->attr_title) . '"' : '';
     $attributes .= ! empty($item->target) ? ' target="' . esc_attr($item->target) . '"' : '';
     $attributes .= ! empty($item->xfn) ? ' rel="' . esc_attr($item->xfn) . '"' : '';
     $attributes .= ! empty($item->url) ? ' href="' . esc_attr($item->url) . '"' : '';
