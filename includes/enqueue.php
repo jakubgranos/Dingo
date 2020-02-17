8acf162276dfcc9d4b12abb06c1af20291f50642
Jakub <jakubgranos@gmail.com>
2020-02-17 12:40:32 +0100

    introduction of prelimiary styles and js

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -6,7 +6,13 @@
   function dingo_load_scripts() {
     // css / scss / others
     wp_enqueue_style('dingo_css', get_template_directory_uri() . './css/dingo.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('bootstrap_css', get_template_directory_uri() . './css/bootstrap.min.css', array(), '4.3.1', 'all');
+    wp_enqueue_style('all_css', get_template_directory_uri() . './css/all.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('dingo_style_css', get_template_directory_uri() . './css/style.css', array(), '1.0.0', 'all');
     // js / jquery / others
-    wp_enqueue_script('dingo_js', get_template_directory_uri() . './js/dingo.js', array(), '1.0.0', true);
+    wp_enqueue_script('dingo_js', get_template_directory_uri() . './js/dingo.js', array('j'), '1.0.0', true);
+    wp_enqueue_script('bootstrap_js', get_template_directory_uri() . './js/boostrap.min.js', array('jQuery'), '4.3.1', true);
+    wp_enqueue_script('jquery-3.3.1_js', get_template_directory_uri() . './js/jquery-3.3.1.slim.min.js', array('jQuery'), '3.3.1', true);
+    wp_enqueue_script('jquery.magnific-popup_js', get_template_directory_uri() . './js/jquery.magnific-popup.js', array('jQuery'), '1.1.0', true);
   }
   add_action('wp_enqueue_scripts', 'dingo_load_scripts');
--- theme-parts/header/header.php
+++ theme-parts/header/header.php
@@ -4,14 +4,77 @@
   @package Dingo
 */
 $logo = get_field('theme_logo', 'option');
-$button = get_field('theme_nav_button', 'option');?>
-
-<header class="header">
-  <img class="header-logo" src="<?php echo $logo;?>" alt="Dingo logo">
-  <nav class="header-navigation">
-    <?php wp_nav_menu(array(
-      'theme_location' => 'header_menu'
-    ));?>
-  </nav>
-  <a class="btn btn-book-table" href="<?php $button?>">Book Table</a>
+$button = get_field('theme_nav_button', 'option');
+$walker = new  Dingo_Walker_nav;?>
+<header class="header main_menu home_menu">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-12">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <a class="navbar-brand" href="/home"><img src="<?php echo $logo;?>" alt="logo"></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"aria-expanded="false"   aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse main-menu-item justify-content-end heder-nav" id="navbarSupportedContent">
+            <?php wp_nav_menu( array('theme_location' => 'header_menu', 'walker' => $walker) );?>
+          </div>
+          <div class="menu_btn">
+            <a href="<?php echo $button['url'];?>" class="btn_1 d-none d-sm-block"><?php echo $button['title'];?></a>
+          </div>          
+        </nav>
+      </div>
+    </div>
+  </div>
 </header>
+
+<!--<header class="main_menu home_menu">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <a class="navbar-brand" href="index.html"> <img src="img/logo.png" alt="logo"> </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse main-menu-item justify-content-end"
+                            id="navbarSupportedContent">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="index.html">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="about.html">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="food_menu.html">Menu</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="chefs.html">Chefs</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Blog
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="blog.html">Blog</a>
+                                        <a class="dropdown-item" href="single-blog.html">Single blog</a>
+                                        <a class="dropdown-item" href="elements.html">Elements</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="contact.html">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="menu_btn">
+                            <a href="#" class="btn_1 d-none d-sm-block">book a tabel</a>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+-->
