9b06fdf4b8837adb419e0310bc683e042966d761
Jakub <jakubgranos@gmail.com>
2020-02-19 23:15:12 +0100

    change inline style into style tag

--- theme-parts/sections/banner/banner_container.php
+++ theme-parts/sections/banner/banner_container.php
@@ -3,7 +3,25 @@
 Template for banner container part
 @package Dingo
  */
-?>
+$bg_image = get_sub_field('background_image');
+$overlay_image = get_sub_field('backgorund_overlay_image');?>
+<style>
+  .banner_part {
+    background-image: url('<?php echo $bg_image; ?>');
+  }
+
+  @media (max-width: 991px) {
+    .banner_part {
+      height: 700px;
+      background-image: none;
+      background-color: #f0eed4;
+    }
+  }
+
+  .banner_part:after {
+    background-image: url('<?php echo $overlay_image; ?>');
+  }
+</style>
 <section class="banner_part">
   <div class="container">
     <div class="row align-items-center">
