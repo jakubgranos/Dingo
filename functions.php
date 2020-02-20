a83594c83724d07ed1daad634cf49f652abc4e42
Jakub <jakubgranos@gmail.com>
2020-02-20 15:00:33 +0100

    add newline at the end of file

--- functions.php
+++ functions.php
@@ -1,15 +1,15 @@
 <?php
 /*
-  Template for  functions
-  @package Dingo
-*/ 
+Template for  functions
+@package Dingo
+ */
 
-  /*require get_template_directory() . '/includes/enqueue.php';
-  require get_template_directory() . '/includes/theme_options.php';*/
-  foreach (glob(get_template_directory() . "/includes/*.php") as $function) {
-    $function= basename($function);
-    require get_template_directory() . '/includes/' . $function;
-  }
+/*require get_template_directory() . '/includes/enqueue.php';
+require get_template_directory() . '/includes/theme_options.php';*/
+foreach (glob(get_template_directory() . "/includes/*.php") as $function) {
+  $function = basename($function);
+  require get_template_directory() . '/includes/' . $function;
+}
 ?>
 
 
