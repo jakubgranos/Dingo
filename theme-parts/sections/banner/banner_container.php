4503ab40b521d67cbf57a95a845b8b919ab096ad
Jakub <jakubgranos@gmail.com>
2020-02-20 15:04:44 +0100

    add check if is not empty to bg images

--- theme-parts/sections/banner/banner_container.php
+++ theme-parts/sections/banner/banner_container.php
@@ -6,21 +6,24 @@ Template for banner container part
 $bg_image = get_sub_field('background_image');
 $overlay_image = get_sub_field('backgorund_overlay_image');?>
 <style>
-  .banner_part {
-    background-image: url('<?php echo $bg_image; ?>');
-  }
-
-  @media (max-width: 991px) {
+  <?php if (!empty($bg_image)): ?>
     .banner_part {
-      height: 700px;
-      background-image: none;
-      background-color: #f0eed4;
+      background-image: url('<?php echo $bg_image; ?>');
     }
-  }
 
-  .banner_part:after {
-    background-image: url('<?php echo $overlay_image; ?>');
-  }
+    @media (max-width: 991px) {
+      .banner_part {
+        height: 700px;
+        background-image: none;
+        background-color: #f0eed4;
+      }
+    }
+  <?php endif;?>
+  <?php if (!empty($overlay_image)): ?>
+    .banner_part:after {
+      background-image: url('<?php echo $overlay_image; ?>');
+    }
+  <?php endif;?>
 </style>
 <section class="banner_part">
   <div class="container">
