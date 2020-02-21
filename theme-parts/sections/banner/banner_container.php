c1fb20a462fb3b6e76a7986184224b13e95e8a97
Jakub <jakubgranos@gmail.com>
2020-02-21 21:58:03 +0100

    move css code out from php tags

--- theme-parts/sections/banner/banner_container.php
+++ theme-parts/sections/banner/banner_container.php
@@ -5,20 +5,22 @@
 */
 $bg_image = get_sub_field('background_image');
 $overlay_image = get_sub_field('backgorund_overlay_image');?>
+
 <style>
   <?php if (!empty($bg_image)): ?>
     .banner_part {
       background-image: url('<?php echo $bg_image; ?>');
     }
-
-    @media (max-width: 991px) {
-      .banner_part {
-        height: 700px;
-        background-image: none;
-        background-color: #f0eed4;
-      }
-    }
   <?php endif;?>
+  
+  @media (max-width: 991px) {
+    .banner_part {
+      height: 700px;
+      background-image: none;
+      background-color: #f0eed4;
+    }
+  }
+  
   <?php if (!empty($overlay_image)): ?>
     .banner_part:after {
       background-image: url('<?php echo $overlay_image; ?>');
