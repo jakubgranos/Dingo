a003e053baf783c3ad93e74bd8929f30a91f26e6
Jakub <jakubgranos@gmail.com>
2020-02-23 23:36:37 +0100

    add if statment cheking is there a post inside

--- theme-parts/sections/menu/featured_menu_item_container.php
+++ theme-parts/sections/menu/featured_menu_item_container.php
@@ -3,35 +3,38 @@
   Template for menu container 
   @package Dingo
   */
+  $menu = get_sub_field('menu');
   $heading = get_sub_field('menu_heading');
   $subheading = get_sub_field('menu_subheading');
   $overlay = get_sub_field('menu_overlay');
 ?>
-<style>
-  <?php if(!empty($overlay)):?>
-    .exclusive_item_part:after {
-      background: url('<?php echo $overlay;?>')bottom right no-repeat;
-      background-size: 15% 65%;
-    }
-  <?php endif;?>
-</style>
-<section class="exclusive_item_part blog_item_section">
-  <div class="container">
-    <div class="row">
-      <div class="col-xl-5">
-        <div class="section_tittle">
-          <?php if(!empty($heading)):?>
-            <p><?php echo $heading; ?></p>
-          <?php endif;?>
-          
-          <?php if(!empty($subheading)):?>
-            <h2><?php echo $subheading;?></h2>
-          <?php endif;?>
+<?php if(!empty($menu)):?>
+  <style>
+    <?php if(!empty($overlay)):?>
+      .exclusive_item_part:after {
+        background: url('<?php echo $overlay;?>')bottom right no-repeat;
+        background-size: 15% 65%;
+      }
+    <?php endif;?>
+  </style>
+  <section class="exclusive_item_part blog_item_section">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-5">
+          <div class="section_tittle">
+            <?php if(!empty($heading)):?>
+              <p><?php echo $heading; ?></p>
+            <?php endif;?>
+
+            <?php if(!empty($subheading)):?>
+              <h2><?php echo $subheading;?></h2>
+            <?php endif;?>
+          </div>
         </div>
       </div>
+      <div class="row">
+        <?php require_once 'featured_menu_items.php';?>
+      </div>
     </div>
-    <div class="row">
-      <?php require_once 'featured_menu_items.php';?>
-    </div>
-  </div>
-</section>
+  </section>
+<?php endif;?>
