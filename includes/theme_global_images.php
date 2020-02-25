732a0f67d38578e979a532f4473cee6c5c0319c0
Jakub <jakubgranos@gmail.com>
2020-02-25 21:20:10 +0100

    add file theme global images and put in new function

---
+++ includes/theme_global_images.php
@@ -0,0 +1,12 @@
+<?php
+/*
+  Theme support file
+  @package Dingo
+*/
+
+// fuctnion for grab source and alt for global use  
+function get_images_attr($field) {
+  $url = $field['url'];
+  $alt = $field['alt'];
+  return "<img src='$url' alt='$alt' />";
+}
