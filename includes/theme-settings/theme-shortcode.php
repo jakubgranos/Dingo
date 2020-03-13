1fff9c571bc05c3d5025b4a08da86417e5eeddf9
Jakub <jakubgranos@gmail.com>
2020-03-13 22:48:11 +0100

    update output shortcode

--- includes/theme-settings/theme-shortcode.php
+++ includes/theme-settings/theme-shortcode.php
@@ -7,19 +7,20 @@
 function dingo_button_shortcode( $atts, $content = null ) {
   //this is for button icon, change in panel
   $icon = get_field( 'icon', 'option' );
-  $get_icon = $icon[ 'url' ];
+  $get_icon_url = $icon[ 'url' ];
   $get_icon_alt = $icon[ 'alt' ];
 
   //this is for default value of href
   $link = get_sub_field( 'link' );
-  $get_link = $link[ 'url' ];
+  $get_link_url = $link[ 'url' ];
 
   $values = shortcode_atts( array(
-    'url' => $get_link,
+    'url' => $get_link_url,
     'target' => ''
   ), $atts );
 
-  return '<a href="' . $values[ 'url' ] . '" target="' . $values[ 'target' ] . '" class="btn_3"> ' . $content . ' <img src="' . $get_icon . '" alt="' . $get_icon_alt . '"> </a> ';
+  $output = "<a href='{$values[ 'url' ]}' target='{$values[ 'target' ]}' class='btn_3'>{$content} <img src='{$get_icon_url}' alt='{$get_icon_alt}'></a>";
+  return $output;
 }
 
 add_shortcode( 'button', 'dingo_button_shortcode' );
