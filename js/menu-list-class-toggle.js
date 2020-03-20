dd60a39e52ac436021bd79cada38d96862d6aada
Jakub <jakubgranos@gmail.com>
2020-03-20 19:42:07 +0100

    delete weird space in classlist

--- js/menu-list-class-toggle.js
+++ js/menu-list-class-toggle.js
@@ -4,16 +4,14 @@ const menu_list_nav = document.querySelectorAll('.menu_list_nav');
 // grab posts with class .tab-pan
 const menu_list_content = document.querySelectorAll('.tab-pane');
 
-//check if menu list nav is not empty
 if (menu_list_nav) {
   const menu_list_nav_prime = menu_list_nav[0];
-  menu_list_nav_prime.classList = 'active';
+  menu_list_nav_prime.classList.add('active');
 }
 
-//check if menu tab pane is not empty
 if (menu_list_content) {
   const menu_list_content_prime = menu_list_content[0];
-  menu_list_content_prime.classList += ' ' + 'show active';
+  menu_list_content_prime.classList.add('active', 'show');
 }
 
 menu_list_nav.onclick = function active() {
