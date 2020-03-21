ae29d482bda06d8bb660103707af2e2ec7939fc7
Jakub <jakubgranos@gmail.com>
2020-03-21 23:43:27 +0100

    update way to display menu-list

--- theme-parts/sections/menu-list/menu-list-navigation.php
+++ theme-parts/sections/menu-list/menu-list-navigation.php
@@ -3,25 +3,26 @@
   Template for menu list navigation
   @package Dingo
 */
-
-$button = get_field( 'button', 'option' );
-$divisions_list_args = array(
+$division_args = array(
   'order' => 'DESC',
   'taxonomy' => 'divisions_list'
 );
-$divisions_list = get_terms( $divisions_list_args );?>
+$divisions = get_terms( $division_args );
+$button = get_field( 'button', 'option' );?>
 
 <div class="col-lg-6">
   <div class="nav nav-tabs food_menu_nav" id="myTab" role="tablist"> <?php
-    foreach( $divisions_list as $division_list ):
-      $division_list_slug = $division_list->slug; 
-      $division_list_name = $division_list->name;?>
+    if( !empty( $divisions ) ):
+      foreach( $divisions as $key => $division ):
+        $division_slug = $division->slug; 
+        $division_name = $division->name;?>
 
-      <a class="menu_list_nav" id="<?php echo $division_list_slug?>-tab" data-toggle="tab" href="#<?php echo $division_list_slug?>" role="tab" 
-      aria-controls="<?php echo $division_list_slug?>" aria-selected="false">
-        <?php echo $division_list_name?>
-        <img src="<?php echo $button;?>" alt="play">
-      </a> <?php
-    endforeach;?>
+        <a class="menu_list_nav <?php if ($key === array_key_first($divisions)) { echo "active"; } ?>" id="<?php echo $division_slug?>-tab"
+        data-toggle="tab" href="#<?php echo $division_slug?>" role="tab" aria-controls="<?php echo $division_slug?>" aria-selected="false">
+          <?php echo $division_name?>
+          <img src="<?php echo $button;?>" alt="play" >
+        </a> <?php
+      endforeach; 
+    endif;?>
   </div>
 </div>
--- theme-parts/sections/menu-list/menu-list.php
+++ theme-parts/sections/menu-list/menu-list.php
@@ -3,28 +3,31 @@
   Template for menu list
   @package Dingo
 */
-$divisions_list_args = array(
+$division_args = array(
   'order' => 'DESC',
   'taxonomy' => 'divisions_list'
 );
-$divisions_list = get_terms( $divisions_list_args );?>
+$divisions = get_terms( $division_args );?>
 
 <div class="col-lg-12">
   <div class="tab-content" id="myTabContent"><?php
-    if( !empty( $divisions_list ) ):
-      foreach( $divisions_list as $division_list ):
-        $division_list_slug = $division_list->slug;
+    if( !empty( $divisions ) ):
+      foreach( $divisions as $key => $division ):
+        $division_slug = $division->slug;
         $query = new WP_Query(array(
           'post_type' => 'menu_item',
           'tax_query' => array(
             array(
               'taxonomy' => 'divisions_list',
               'field'    => 'slug',
-              'terms'    => $division_list_slug,
+              'terms'    => $division_slug,
             ),
           ),
-        )); 
-        require 'menu-list-content.php';
+        ));?>
+        <div class="tab-pane fade single-member <?php if ($key === array_key_first($divisions)) { echo "active show"; } ?>" 
+        id="<?php echo $division_slug;?>" role="tabpanel" aria-labelledby="<?php echo $division_slug;?>-tab">
+          <?php require 'menu-list-content.php'; ?>
+        </div> <?php
         wp_reset_postdata();
       endforeach;
     endif;?>
