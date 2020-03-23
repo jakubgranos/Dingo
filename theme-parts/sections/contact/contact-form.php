01a9cca5fe78618289d88328be5d1674b2c0b131
Jakub <jakubgranos@gmail.com>
2020-03-23 01:27:26 +0100

    add contact section

---
+++ theme-parts/sections/contact/contact-form.php
@@ -0,0 +1,53 @@
+<?php
+/*
+  Template for contact form
+  @package Dingo
+*/
+?>
+
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <input type="email" class="form-control" id="inputEmail4" placeholder="Name *">
+    </div>
+    <div class="form-group col-md-6">
+      <input type="password" class="form-control" id="inputPassword4" placeholder="Email address *">
+    </div>
+    <div class="form-group col-md-6">
+      <select class="form-control" id="Select">
+        <option value="1" selected>Persons *</option>
+        <option value="2">Number of guests 1</option>
+        <option value="3">Number of guests 2</option>
+        <option value="4">Number of guests 3</option>
+        <option value="5">Number of guests 4</option>
+      </select>
+    </div>
+    <div class="form-group col-md-6">
+      <input type="text" class="form-control" id="pnone" placeholder="Phone number *">
+    </div>
+    <div class="form-group col-md-6">
+      <div class="input-group date">
+        <input id="datepicker" type="text" class="form-control" placeholder="Date *" />
+      </div>
+      </div>
+    <div class="form-group col-md-6">
+      <select class="form-control" id="Select2">
+        <option value="" selected>Time *</option>
+        <option value="1">8 AM TO 10AM</option>
+        <option value="1">10 AM TO 12PM</option>
+        <option value="1">12PM TO 2PM</option>
+        <option value="1">2PM TO 4PM</option>
+        <option value="1">4PM TO 6PM</option>
+        <option value="1">6PM TO 8PM</option>
+        <option value="1">4PM TO 10PM</option>
+        <option value="1">10PM TO 12PM</option>
+      </select>
+    </div>
+    <div class="form-group col-md-12">
+      <textarea class="form-control" id="Textarea" rows="4" placeholder="Your Note *"></textarea>
+    </div>
+  </div>
+  <div class="regerv_btn">
+    <a href="#" class="btn_4">Book A Table</a>
+  </div>
+</form>
---
+++ theme-parts/sections/contact/contact.php
@@ -0,0 +1,40 @@
+<?php
+/*
+  Template for contact
+  @package Dingo
+*/
+$heading = get_sub_field( 'heading' );
+$subheading = get_sub_field( 'subheading' );
+$backgorund = get_sub_field( 'backgorund' );?>
+
+<style> <?php 
+  if ( !empty( $heading ) ): ?>
+    .regervation_part {
+      background-image: url('<?php echo $backgorund?>');
+    } <?php
+  endif;?>
+</style>
+<section class="regervation_part section_padding">
+  <div class="container">
+    <div class="row">
+      <div class="col-xl-5">
+        <div class="section_tittle"> <?php 
+          if( !empty($heading) ): ?>
+            <p><?php echo $heading;?></p> <?php
+          endif;
+
+          if( !empty($subheading) ): ?>
+            <h2><?php echo $subheading;?></h2> <?php
+          endif;?>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="regervation_part_iner">
+          <?php require 'contact-form.php';?>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
