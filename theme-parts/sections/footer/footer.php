2fc7e6eb18ad6e2a634569328cf6f1536c2f978e
Jakub <jakubgranos@gmail.com>
2020-03-27 06:09:24 +0100

    add wp code in footer

--- theme-parts/sections/footer/footer.php
+++ theme-parts/sections/footer/footer.php
@@ -1,44 +1,43 @@
+<?php
+/*
+  Theme banner footer
+  @package Dingo
+*/ 
+$copyright = get_field( 'copyright', 'option' );?>
 <footer class="footer-area">
   <div class="container">
-    <div class="row">
-      <div class="col-xl-3 col-sm-6 col-md-3 col-lg-3">
-        <div class="single-footer-widget footer_1">
-          <h4>About Us</h4>
-          <p>Heaven fruitful doesn't over for these theheaven fruitful doe over days appear creeping seasons sad behold beari ath of it fly signs bearing be one blessed after.</p>
-        </div>
-      </div>
-      <div class="col-xl-3 col-sm-6 col-md-2 col-lg-3">
-        <div class="single-footer-widget footer_2">
-          <h4>Important Link</h4>
-        <div class="contact_info">
-          <ul>
-            <li><a href="#">WHMCS-bridge</a></li>
-            <li><a href="#"> Search Domain</a></li>
-            <li><a href="#">My Account</a></li>
-            <li><a href="#">Shopping Cart</a></li>
-            <li><a href="#"> Our Shop</a></li>
-          </ul>
-        </div>
-      </div>
+    <div class="row"> <?php
+      if( have_rows( 'row', 'option' ) ):
+        while( have_rows( 'row', 'option' ) ): the_row();
+          $title = get_sub_field( 'title' ); 
+          $content = get_sub_field( 'content' )?>
+          <div class="col-xl-3 col-sm-6 col-md-3 col-lg-3"> 
+            <div class="single-footer-widget footer_1">
+              <h4><?php echo $title?></h4>
+              <p><?php echo $content;?></p>
+            </div>
+          </div> <?php
+        endwhile;
+      endif;?>
     </div>
-  </div>
-  <div class="copyright_part_text">
-    <div class="row">
-      <div class="col-lg-8">
-        <p class="footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-          Copyright ©<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="ti-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-          re-create by: Jakub Granos<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-        </p>
-      </div>
-      <div class="col-lg-4">
-        <div class="copyright_social_icon text-right">
-          <a href="#"><i class="fab fa-facebook-f"></i></a>
-          <a href="#"><i class="fab fa-twitter"></i></a>
-          <a href="#"><i class="ti-dribbble"></i></a>
-          <a href="#"><i class="fab fa-behance"></i></a>
+    <div class="copyright_part_text">
+      <div class="row">
+        <div class="col-lg-8">
+          <?php echo $copyright;?>
+        </div>
+        <div class="col-lg-4">
+          <div class="copyright_social_icon text-right"> <?php
+            if( have_rows( 'social_media', 'option' ) ):
+              while( have_rows( 'social_media', 'option' ) ): the_row();
+                $url = get_sub_field( 'url' ); 
+                $icon = get_sub_field( 'icon' );?>
+                <a href="<?php echo $url[ 'url' ]?>"><i class="ti-<?php echo $icon;?>"></i></a> <?php
+              endwhile;
+            endif;?>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   </div>
 </footer>
+
